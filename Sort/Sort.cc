87e566d7af5c09476fb4119461108155a93fb5b5
JW1992 <jiaweix@umich.edu>
2018-01-15 14:53:15 -0500

    Quick sort (partition)

--- Sort/Sort.cc
+++ Sort/Sort.cc
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
+
 void PrintArray(std::vector<int>& nums){
 	for(int i=0; i<nums.size(); i++){
 		std::cout<<nums[i]<<" ";
 	}
 	std::cout<<std::endl;
 }
+void Swap(std::vector<int>& nums, int m, int n){
+	int nTemp = nums[m];
+	nums[m] = nums[n];
+	nums[n] = nTemp;
+}
+
 
 //Heap sort
 //Merge sort
@@ -29,15 +36,32 @@ void MergeSort(std::vector<int>& nums, int m, int n){
 	MergeSort(nums, medium, n);
 	Merge(nums, m, medium, n);
 }
+
 //Quick sort
-
-
+int Partition(std::vector<int>& nums, int m, int n){
+	int pivot = nums[n-1];
+	int i = m-1;//Index of the value that is LARGER than pivot.
+	for(int j=m; j<n-1; j++){
+		if(nums[j]<=pivot){
+			i++;
+			Swap(nums, j, i);
+		}
+	}
+	Swap(nums, ++i, n-1);
+	return i;
+}
+void QuickSort(std::vector<int>& nums, int m, int n){
+	if(n-m<=1) return;
+	int medium = Partition(nums, m, n);
+	QuickSort(nums, m, medium);
+	QuickSort(nums, medium+1, n);
+}
 
 int main(){
 	std::vector<int> nums = {9,8,7,6,5,4,3,2,1,0};
 	PrintArray(nums);
-	MergeSort(nums, 0, nums.size());
-	//Merge(nums, 0,10,10);
+	//MergeSort(nums, 0, nums.size());
+	QuickSort(nums, 0, nums.size());
 	PrintArray(nums);
 	return 0;
 }
